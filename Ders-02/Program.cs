586d89837134c29c2fd8e5744b35aaded82afd96
Erdem Bicer <bicererdem@hotmail.de>
2024-12-17 21:28:52 +0300

    Ders 2 yapıldı.

---
+++ Ders-02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Double
+
+            //double number;
+
+            //number = 4.85;
+
+            //Console.WriteLine(number);
+
+            Console.WriteLine("***** Fiyat Listesi *****");
+            Console.WriteLine();
+
+            double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            applePrice = 14.85;
+            orangePrice = 20.95;
+            strawberryPrice = 45;
+            potatoPrice = 9.74;
+            tomatoPrice = 6.88;
+
+            Console.WriteLine("---- Elma Birim Fiyatı: " + applePrice + " TL");
+            Console.WriteLine("---- Portakal Birim Fiyatı: " + orangePrice + " TL");
+            Console.WriteLine("---- Çilek Birim Fiyatı: " + strawberryPrice + " TL");
+            Console.WriteLine("---- Patates Birim Fiyatı: " + potatoPrice + " TL");
+            Console.WriteLine("---- Domates Birim Fiyatı: " + tomatoPrice + " TL");
+
+            double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            appleGram = 1.245;
+            orangeGram = 2.650;
+            strawberryGram = 0.750;
+            potatoGram = 4.859; 
+            tomatoGram = 3.745;
+
+            double appleTotalPrice = appleGram * applePrice;
+            double orangeTotalPrice = orangeGram * orangePrice;
+            double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            double potatoTotalPrice = potatoPrice * potatoGram;
+            double tomatoTotalPrice = tomatoPrice * tomatoGram;
+
+            Console.WriteLine();
+            Console.WriteLine("Elmanın toplam fiyatı: " + appleTotalPrice);
+            Console.WriteLine("Portakalın toplam fiyatı: " + orangeTotalPrice);
+            Console.WriteLine("Çileğin toplam fiyatı: " + strawberryTotalPrice);
+            Console.WriteLine("Patatesin toplam fiyatı: " + potatoTotalPrice);
+            Console.WriteLine("Domatesin toplam fiyatı: " + tomatoTotalPrice);
+
+            double total = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
+
+            Console.WriteLine();
+            Console.WriteLine("Genel toplam: " + total);
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
---
+++ Ders-02/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ders-02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ders-02")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1f2ef243-9a4a-4a1e-8b72-225efb5fc1bc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
