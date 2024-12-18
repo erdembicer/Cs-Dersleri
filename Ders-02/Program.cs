f4f05a0b38f8e1e5b43dda450fa23c7b80311c45
Erdem Bicer <bicererdem@hotmail.de>
2024-12-18 14:05:45 +0300

    İkinci ders. 32'inci dk.

--- Ders-02/Program.cs
+++ Ders-02/Program.cs
@@ -19,48 +19,92 @@ namespace Ders_02
 
             //Console.WriteLine(number);
 
-            Console.WriteLine("***** Fiyat Listesi *****");
-            Console.WriteLine();
+            //Console.WriteLine("***** Fiyat Listesi *****");
+            //Console.WriteLine();
 
-            double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
 
-            applePrice = 14.85;
-            orangePrice = 20.95;
-            strawberryPrice = 45;
-            potatoPrice = 9.74;
-            tomatoPrice = 6.88;
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
 
-            Console.WriteLine("---- Elma Birim Fiyatı: " + applePrice + " TL");
-            Console.WriteLine("---- Portakal Birim Fiyatı: " + orangePrice + " TL");
-            Console.WriteLine("---- Çilek Birim Fiyatı: " + strawberryPrice + " TL");
-            Console.WriteLine("---- Patates Birim Fiyatı: " + potatoPrice + " TL");
-            Console.WriteLine("---- Domates Birim Fiyatı: " + tomatoPrice + " TL");
+            //Console.WriteLine("---- Elma Birim Fiyatı: " + applePrice + " TL");
+            //Console.WriteLine("---- Portakal Birim Fiyatı: " + orangePrice + " TL");
+            //Console.WriteLine("---- Çilek Birim Fiyatı: " + strawberryPrice + " TL");
+            //Console.WriteLine("---- Patates Birim Fiyatı: " + potatoPrice + " TL");
+            //Console.WriteLine("---- Domates Birim Fiyatı: " + tomatoPrice + " TL");
 
-            double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
 
-            appleGram = 1.245;
-            orangeGram = 2.650;
-            strawberryGram = 0.750;
-            potatoGram = 4.859; 
-            tomatoGram = 3.745;
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859; 
+            //tomatoGram = 3.745;
 
-            double appleTotalPrice = appleGram * applePrice;
-            double orangeTotalPrice = orangeGram * orangePrice;
-            double strawberryTotalPrice = strawberryGram * strawberryPrice;
-            double potatoTotalPrice = potatoPrice * potatoGram;
-            double tomatoTotalPrice = tomatoPrice * tomatoGram;
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double potatoTotalPrice = potatoPrice * potatoGram;
+            //double tomatoTotalPrice = tomatoPrice * tomatoGram;
 
-            Console.WriteLine();
-            Console.WriteLine("Elmanın toplam fiyatı: " + appleTotalPrice);
-            Console.WriteLine("Portakalın toplam fiyatı: " + orangeTotalPrice);
-            Console.WriteLine("Çileğin toplam fiyatı: " + strawberryTotalPrice);
-            Console.WriteLine("Patatesin toplam fiyatı: " + potatoTotalPrice);
-            Console.WriteLine("Domatesin toplam fiyatı: " + tomatoTotalPrice);
+            //Console.WriteLine();
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyat: " + applePrice + " - Miktar: " + appleGram + " - Toplam tutar: " + appleTotalPrice);
+            //Console.WriteLine("Alınan ürün: Portakal - " + "Birim fiyat: " + orangePrice + " - Miktar: " + orangeGram + " - Toplam tutar: " + orangeTotalPrice);
+            //Console.WriteLine("Alınan ürün: Çilek - " + "Birim fiyat: " + strawberryPrice + " - Miktar: " + strawberryGram + " - Toplam tutar: " + strawberryTotalPrice);
+            //Console.WriteLine("Alınan ürün: Patates - " + "Birim fiyat: " + potatoPrice + " - Miktar: " + potatoGram + " - Toplam tutar: " + potatoTotalPrice);
+            //Console.WriteLine("Alınan ürün: Domates - " + "Birim fiyat: " + tomatoPrice + " - Miktar: " + tomatoGram + " - Toplam tutar: " + tomatoTotalPrice);
 
-            double total = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
 
-            Console.WriteLine();
-            Console.WriteLine("Genel toplam: " + total);
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("Genel toplam: " + shoppingTotalPrice + " TL");
+            #endregion
+
+            #region Char Değişkenler
+
+            //char symbol;
+            //symbol = 'a';
+
+            //Console.WriteLine(symbol);
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri String Değişkenler
+
+            //Console.WriteLine("***** CSharp Hava Yolları Yolcu Bilgisi *****");
+            //Console.WriteLine();
+
+            //string passengerName, passengerSurname, passengerCity, passengerAge, passengerIDNumber;
+
+            //Console.Write("Yolcu adı: ");
+            //passengerName = Console.ReadLine();
+            //Console.Write("Yolcu Soyadı: ");
+            //passengerSurname = Console.ReadLine();
+            //Console.Write("Yolcu Adresi: ");
+            //passengerCity = Console.ReadLine();
+            //Console.Write("Yolcu Yaşı: ");
+            //passengerAge = Console.ReadLine();
+            //Console.Write("Yolcu Kimlik No: ");
+            //passengerIDNumber = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("------------");
+            //Console.WriteLine("Yolcu Kimlik No " + passengerIDNumber + " Yolcu Ad Soyad: " + passengerName + " " + passengerSurname + " " + passengerAge + " " + passengerCity);
+
+
+            #endregion
+
+            #region Klaveyden Tam Sayı Girişleri ve Dönüşümler
+
+
+
+
+
             #endregion
 
             Console.Read();
